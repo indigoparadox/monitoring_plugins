f1126d80005288b6166fac1e11b84bc77eb74fe7
indigoparadox <indigo.repo@interfinitydynamics.info>
2021-10-14 19:52:36 -0400

    Initial commit.

---
+++ cmk/agent/default_tr.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+echo "<<<default_tr>>>"
+
+traceroute -w 1 -q 1 google.com
+
---
+++ cmk/plugins/default_tr.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+from .agent_based_api.v1 import *
+
+def discover_default_tr( section ):
+    yield Service()
+
+def check_default_tr( section ):
+    hop_name_found = False
+    for line in section:
+        if SEARCH_TR_HOP in line[1]:
+            yield Result( state=State.OK, summary='Found hop: {} {} ({}ms)'.format( line[1], line[2], line[3] ) )
+            return
+    yield Result( state=State.CRIT, summary='Could not find hop: {}'.format( SEARCH_TR_HOP ) )
+
+register.check_plugin(
+    name="default_tr",
+    service_name="Default Traceroute",
+    discovery_function=discover_default_tr,
+    check_function=check_default_tr
+)
+
