78163ee9393b5d9fd62e84ece788b75f91f442c9
indigoparadox <indigo.repo@interfinitydynamics.info>
2022-03-02 13:30:31 -0500

    Added config for mk_git.

--- cmk/plugins/mk_git.py
+++ cmk/plugins/mk_git.py
@@ -7,9 +7,10 @@ def discover_mk_git( section ):
         if '/' == line[0][0]:
             yield Service( item=line[0] )
 
-def check_mk_git( item, section ):
+def check_mk_git( item, params, section ):
     in_section = False
     mod_count = 0
+    new_count = 0
     for line in section:
         if item == line[0]:
             # Line is the sought repo name.
@@ -19,23 +20,41 @@ def check_mk_git( item, section ):
             # Line is a new repo name.
             in_section = False
 
-        elif in_section:
+        elif in_section and '?' == line[0][0]:
+            # Line is a filename modified in the repo.
+            new_count += 1
+
+        elif in_section and '!' == line[0][0]:
             # Line is a filename modified in the repo.
             mod_count += 1
 
-    if 0 == mod_count:
-        yield Result(
-            state=State.OK,
-            summary='No modified files in repo.' )
+    # Figure out the message.
+    summary_msg = ''
+    if 0 == mod_count and 0 == new_count:
+        summary_msg = 'No new or modified files'
     else:
-        yield Result(
-            state=State.WARN,
-            summary='{} modified files in repo'.format( mod_count ) )
+        if 0 < mod_count:
+            summary_msg += '{} modified files'.format( mod_count )
+        if 0 < new_count:
+            summary_msg += '{} new files'.format( new_count )
+
+    if params['mod_crit'] <= mod_count or params['new_crit'] <= new_count:
+        yield Result( state=State.CRIT, summary=summary_msg )
+    elif params['mod_warn'] <= mod_count or params['new_warn'] <= new_count:
+        yield Result( state=State.WARN, summary=summary_msg )
+    else:
+        yield Result( state=State.OK, summary=summary_msg )
     
 register.check_plugin(
     name="mk_git",
     service_name="git Repository %s",
     discovery_function=discover_mk_git,
-    check_function=check_mk_git
+    check_function=check_mk_git,
+    check_default_parameters={
+        'mod_warn': 1,
+        'mod_crit': 5,
+        'new_warn': 1,
+        'new_crit': 5 },
+    check_ruleset_name="mk_git"
 )
 
---
+++ cmk/wato/mk_git_parameters.py
@@ -0,0 +1,37 @@
+
+from cmk.gui.i18n import _
+from cmk.gui.valuespec import (
+    Dictionary,
+    Integer,
+    TextAscii
+)
+from cmk.gui.plugins.wato import (
+    CheckParameterRulespecWithoutItem,
+    rulespec_registry,
+    RulespecGroupCheckParametersOperatingSystem
+)
+
+def _parameter_valuespec_mk_git():
+    return Dictionary(
+        elements=[
+            ("new_warn", 
+                Integer( title=_( 'Warning above untracked files' ) )),
+            ("new_crit",
+                Integer( title=_( 'Critical above untracked files' ) )),
+            ("mod_warn", 
+                Integer( title=_( 'Warning above modified files' ) )),
+            ("mod_crit",
+                Integer( title=_( 'Critical above modified files' ) ))
+        ],
+    )
+
+rulespec_registry.register(
+    CheckParameterRulespecWithoutItem(
+        check_group_name="mk_git",
+        group=RulespecGroupCheckParametersOperatingSystem,
+        match_type="dict",
+        parameter_valuespec=_parameter_valuespec_mk_git,
+        title=lambda: _( 'git respository changes' )
+    )
+)
+
