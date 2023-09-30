f7da566a5d9b73abec0c780de2d2bafcb7e81a13
indigoparadox <indigo.repo@interfinitydynamics.info>
2023-09-30 16:58:05 -0400

    Add cache for missing SMART data.

--- cmk/plugins/mk_snmpsmart.py
+++ cmk/plugins/mk_snmpsmart.py
@@ -10,7 +10,8 @@ from .agent_based_api.v1 import (
     State,
     Metric,
     render,
-    OIDEnd
+    OIDEnd,
+    get_value_store
 )
 
 register.snmp_section(
@@ -37,8 +38,27 @@ def discover_mk_snmpsmart( section ):
             yield Service( item=i[1] )
             found_devs.append( i[1] )
 
+def check_mk_snmpsmart_thresh( val_cur, val_worst, val_threshold ):
+    
+    ''' Evaluate thresholds. '''
+
+    status_text = 'Value OK'
+    status = State.OK
+    if val_worst < val_threshold:
+        status_text = 'Worst value below threshold'
+        status = State.WARN
+    if val_cur < val_threshold:
+        status_text = 'Current value below threshold'
+        status = State.CRIT
+
+    return status, status_text
+
 def check_mk_snmpsmart( item, params, section ):
 
+    pv = get_value_store()
+    report_success = []
+    if not item in pv:
+        pv[item] = {}
     for section_i in section:
 
         # Skip other device data.
@@ -46,43 +66,59 @@ def check_mk_snmpsmart( item, params, section ):
         if device != item:
             continue
 
+        # Fail and keep moving on bad lines.
         if 'echo: write error on stdout' in section_i:
             return
 
         if '' in section_i:
             continue
 
+        # Translate section columns to variables.
         attribute = section_i[2]
         val_cur = int( section_i[3] ) if '' != section_i[3] else -1
         val_worst = int( section_i[4] ) if '' != section_i[4] else -1
         val_threshold = int( section_i[5] ) if '' != section_i[5] else -1
-        val_raw = -1
-        try:
-            val_raw = int( section_i[6] )
-        except ValueError:
-            pass
+        val_raw = int( section_i[6] )
 
-        # Evaluate thresholds.
-        status_text = 'Value OK'
-        status = State.OK
-        if 0 <= val_worst and val_worst < val_threshold:
-            status_text = 'Worst value below threshold'
-            status = State.WARN
-        if 0 <= val_cur and val_cur < val_threshold:
-            status_text = 'Current value below threshold'
-            status = State.CRIT
+        assert 0 <= val_cur
+        assert 0 <= val_worst
+        assert 0 <= val_threshold
+        assert 0 <= val_raw
+        assert '' != attribute
 
-        if 0 <= val_raw:
-            yield Metric( attribute, val_cur )
+        status, status_text = \
+            check_mk_snmpsmart_thresh( val_cur, val_worst, val_threshold )
 
-        if 0 <= val_cur and 0 <= val_worst:
+        yield Metric( attribute, val_cur )
+
+        yield Result( state=status,
+            summary='{}: {} (Value: {}, Worst: {}, Raw: {})'.format(
+                attribute, status_text, val_cur, val_worst, val_raw ) )
+
+        # Report was successful, so cache value and add to success list.
+        report_success.append( attribute )
+        if not attribute in pv[device]:
+            pv[device][attribute] = {
+                'value': val_cur,
+                'val_worst': val_worst,
+                'val_raw': val_raw,
+                'val_threshold': val_threshold }
+
+    # Report cached values for any missing from the last stanza.
+    # TODO: Expire cache based on age?
+    for attribute in pv[item]:
+        if not attribute in report_success:
+            status, status_text = check_mk_snmpsmart_thresh(
+                pv[device][attribute]['value'],
+                pv[device][attribute]['val_worst'],
+                pv[device][attribute]['val_threshold'] )
             yield Result( state=status,
-                summary='{}: {} (Value: {}, Worst: {}{})'.format(
-                    attribute, status_text, val_cur, val_worst,
-                    ', Raw: {}'.format( val_raw ) if 0 <= val_raw else '' ) )
-        else:
-            yield Result( state=State.UNKNOWN,
-                summary='{}: Invalid value'.format( attribute ) )
+                summary='{} (Cached): {} (Value: {}, Worst: {}, Raw: {})'.format(
+                    attribute,
+                    status_text,
+                    pv[device][attribute]['value'],
+                    pv[device][attribute]['val_worst'],
+                    pv[device][attribute]['val_raw'] ) )
 
 register.check_plugin(
     name="mk_snmpsmart",
